61b5aeb0ac731cc91252e6906f688d14858e0777
Davi-Hilario <davi.hilario@sptech.school>
2023-08-20 17:51:01 -0300

    definindo funções e comandos de excluir e alterar

--- Conexao_MySql/conexao.py
+++ Conexao_MySql/conexao.py
@@ -7,86 +7,120 @@ try:
     conexao = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="senha",
+        password="Anfitras01;!",
         port=3306,
         database="ConexaoMysqlPython"
     )
-
-    print(conexao)
 except mysql.connector.Error as err:
     print(f'Erro na conexao com o BD: {err}')
+else:
+    print("conexao iniciada!")
+    comando = conexao.cursor()
 
 #realizando comandos mysql
-comando = conexao.cursor()
 
-#criando tabela Pessoa
-try:
-    executar = comando.execute("create table Pessoa"
-                    "("
-                        "id int primary key auto_increment,"
-                        "nome varchar(50),"
-                        "datanasc date,"
-                        "sexo char(1)"
-                    ");"
-    )
-    # print(executar)
-except mysql.connector.Error as err:
-    if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
-        print("Essa tabela já existe!")
+def criarTabela():
+    #criando tabela Pessoa
+    try:
+        comando.execute("create table Pessoa"
+                        "("
+                            "id int primary key auto_increment,"
+                            "nome varchar(50),"
+                            "datanasc date,"
+                            "sexo char(1)"
+                        ");"
+        )
+        # print(executar)
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+            print("Essa tabela já existe!")
+        else:
+            print(err.msg)
     else:
-        print(err.msg)
-else:
-    print("OK")
+        print("Tabela criada!")
 
-#inserindo dados na tabela Pessoa
-try:
-    executar = comando.execute("Insert into Pessoa (nome, datanasc, sexo) values " 
-                    "('Pedro', '2004-02-20','M'),"
-                    "('Maria', '2002-06-12','F'),"
-                    "('Joaquim', '2000-01-04','M'),"
-                    "('Larissa', '2006-09-24','F'),"
-                    "('Manoel', '1994-12-01','M');"
-    )
-    # print(executar)
-    conexao.commit() #enviando dados pro mysql
-except mysql.connector.Error as err:
-    print(f"Erro ao inserir registro: {err}")
-else:
-    print("Dados inseridos com sucesso!")
+def inserirDados():
+    #inserindo dados na tabela Pessoa
+    try:
+        executar = comando.execute("Insert into Pessoa (nome, datanasc, sexo) values " 
+                        "('Pedro', '2004-02-20','M'),"
+                        "('Maria', '2002-06-12','F'),"
+                        "('Joaquim', '2000-01-04','M'),"
+                        "('Larissa', '2006-09-24','F'),"
+                        "('Manoel', '1994-12-01','M');"
+        )
+        # print(executar)
+        conexao.commit() #enviando dados pro mysql
+    except mysql.connector.Error as err:
+        print(f"Erro ao inserir registro: {err}")
+    else:
+        print("Dados inseridos com sucesso!")
 
-#Consultando dados da tabela pessoa
-try:
-    dados = {}
-    comando.execute("Select * from Pessoa;")
-    for (id,nome, datanasc, sexo) in comando:
-        dados['id'] = id
-        dados['nome'] = nome
-        dados['datanasc'] = datanasc
-        dados['sexo'] = sexo
+def deletarDados():
+    #Deletando registros
+    try:
+        comando.execute("Delete from Pessoa where id = 1;")
+    except mysql.connector.Error as err:
+        print(f"Erro ao excluir registro: {err}")
+    else:
+        print("Registro excluído com sucesso!")
 
-        #convertendo a data para um formato legivel
+def alterarDados():
+    #Alterando dados da tabela pessoa
+    try:
+        comando.execute("Update Pessoa set nome = 'Davi' where id = 3;")
+    except mysql.connector.Error as err:
+        print(f"Erro ao alterar o registro: {err}")
+        conexao.commit()
+    else:
+        print("Registro alterado com sucesso!")
 
-        print("="*40)
-         # formato padrao
-        print(f"{dados['datanasc']}")
+def consultarDados():
+    #Consultando dados da tabela pessoa
+    try:
+        dados = {}
+        comando.execute("Select * from Pessoa;")
+        for (id,nome, datanasc, sexo) in comando:
+            dados['id'] = id
+            dados['nome'] = nome
+            dados['datanasc'] = datanasc
+            dados['sexo'] = sexo
+            print(dados , "\n\n\n")
 
-        # meses como números (m)
-        print(f"{dados['datanasc']:%d %m %Y}") 
+            #convertendo a data para um formato legivel
+            print("="*40)
+            # formato padrao
+            print(f"{dados['datanasc']}")
 
-        # meses com seus nomes completos (B)
-        print(f"{dados['datanasc']:%d %B %Y}")
+            # meses como números (m)
+            print(f"{dados['datanasc']:%d %m %Y}") 
 
-         # meses com seus nomes abreviados (b)
-        print(f"{dados['datanasc']:%d %b %Y}") 
+            # meses com seus nomes completos (B)
+            print(f"{dados['datanasc']:%d %B %Y}")
 
-        # data com barras (D)
-        print(f"{dados['datanasc']:%D}")
+            # meses com seus nomes abreviados (b)
+            print(f"{dados['datanasc']:%d %b %Y}") 
 
-        # ano abreviado (y)
-        print(f"{dados['datanasc']:%d %b %y}") 
-        print("="*40)
+            # data com barras (D)
+            print(f"{dados['datanasc']:%D}")
 
-except mysql.connector.Error as err:
-    print("Erro ao consultar os dados!")
-else:
-    print("Consulta realizada com sucesso!")
+            # ano abreviado (y)
+            print(f"{dados['datanasc']:%d %b %y}") 
+            print("="*40)
+
+    except mysql.connector.Error as err:
+        print("Erro ao consultar os dados!")
+    else:
+        print("Consulta realizada com sucesso!")
+
+def encerrarConexao():
+    comando.close()
+    conexao.close()
+    print("\n\nConexao finalizada!")
+
+criarTabela()
+inserirDados()
+deletarDados()
+alterarDados()
+consultarDados()
+encerrarConexao()
